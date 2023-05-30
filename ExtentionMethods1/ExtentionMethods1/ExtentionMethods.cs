9badcb71f065af05547762998d06586ee35dd1a1
gulievh <133109621+gulievh@users.noreply.github.com>
2023-05-31 03:42:58 +0400

    Add files via upload

---
+++ ExtentionMethods1/ExtentionMethods1/ExtentionMethods.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethods1
+{
+    internal static class ExtentionMethods
+    {
+        public static bool IsOdd(this int number)
+        {
+            if (number % 2 == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        } 
+        public static bool IsEven(this int number)
+        {
+            if (number % 2 == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool IsContainsDigit(this string str)
+        {
+            bool checkDigit=false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i])) { checkDigit = true; }
+            }
+            return checkDigit;
+            {
+                
+            }
+        }
+       // public static string IsCapitalized(this string str)???
+         // public static string GetValueIndexes(this string str)??
+        
+            
+        
+    }
+}
---
+++ ExtentionMethods1/ExtentionMethods1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ExtentionMethods1
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int a = 23;
+            int b = 24;
+
+            Console.WriteLine(a.IsOdd());
+            Console.WriteLine(b.IsEven());
+
+            
+        }
+    }
+}
